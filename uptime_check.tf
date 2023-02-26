3b4df710c98959e2692aa95e49559fafe7299d44
mydevops1989 <111324846+Sarik89@users.noreply.github.com>
2023-02-26 17:03:16 -0600

    Create uptime_check.tf

---
+++ uptime_check.tf
@@ -0,0 +1,6 @@
+module "uptime_check" {
+  source             = "./modules/monitoring"
+  google_domain_name = var.google_domain_name
+  PROJECT_ID         = var.PROJECT_ID
+  service_name       = "uptime_check"
+}
