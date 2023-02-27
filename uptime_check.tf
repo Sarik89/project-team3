541f3be320bd0e57c8c3a4a5dbbf46a99c14f0a1
mydevops1989 <111324846+Sarik89@users.noreply.github.com>
2023-02-26 20:10:23 -0600

    Update uptime_check.tf

--- uptime_check.tf
+++ uptime_check.tf
@@ -2,5 +2,5 @@ module "uptime_check" {
   source             = "./module/monitoring"
   google_domain_name = var.google_domain_name
   project_id        = var.project_id
-  service_name       = "uptime_check"
+  service_name       = "application"
 }
